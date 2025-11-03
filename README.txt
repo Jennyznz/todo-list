b09e016ede64bf12b3cb9686764a12148df798e4
Jenny <156132099+Jennyznz@users.noreply.github.com>
2025-11-03 13:34:20 -0500

    Update README documentation

---
+++ README.txt
@@ -0,0 +1,14 @@
+To Do List
+- Used JavaScript objects to model projects and tasks.
+- Dynamically rendered the interface with DOM manipulation.
+- Modularized code into separate ES6 modules for clarity and maintainability.
+- Implemented data persistence with the localStorage API.
+- Integrated SVG icons for a clean and responsive design.
+- Bundled and optimized assets using Webpack.
+
+Local Setup:
+git clone https://github.com/Jennyznz/todo-list.git
+cd todo-list
+npm install
+npm run build
+
--- src/index.js
+++ src/index.js
@@ -1,42 +1,45 @@
 import "./styles.css";
-import { createForm, clearForm, allProjects, Project, Task } from "./projectForm.js";
+import {
+  createForm,
+  clearForm,
+  allProjects,
+  Project,
+  Task,
+} from "./projectForm.js";
 import { displayAll } from "./all.js";
-import { projectView } from "./projectView.js"; 
+import { projectView } from "./projectView.js";
 
 function loadProjects() {
-    const storedProjects = localStorage.getItem('projects');
-    if (storedProjects) {
-        const parsedP = JSON.parse(storedProjects);
-        parsedP.forEach(projData => {
-            // Extract project properties
-            const project = new Project(
-                projData.title,
-                projData.description,
-                projData.isPriority,
-                projData.dueDate
-            );
+  const storedProjects = localStorage.getItem("projects");
+  if (storedProjects) {
+    const parsedP = JSON.parse(storedProjects);
+    parsedP.forEach((projData) => {
+      // Extract project properties
+      const project = new Project(
+        projData.title,
+        projData.description,
+        projData.isPriority,
+        projData.dueDate,
+      );
 
-            // Extract all tasks
-            projData.tasks.forEach(taskData => {
-                const task = new Task(
-                    taskData.content,
-                    taskData.isComplete
-                )
-                project.createTask(task);
-            });
+      // Extract all tasks
+      projData.tasks.forEach((taskData) => {
+        const task = new Task(taskData.content, taskData.isComplete);
+        project.createTask(task);
+      });
 
-            allProjects.push(project);
-        });
-    }
+      allProjects.push(project);
+    });
+  }
 }
 loadProjects();
 
 // Add new project
-const addBtn = document.querySelector('#add-btn');
-    addBtn.addEventListener("click", () => {
-        clearForm(); 
-        createForm();
-    });
+const addBtn = document.querySelector("#add-btn");
+addBtn.addEventListener("click", () => {
+  clearForm();
+  createForm();
+});
 
 // Default is list of projects
 displayAll();
@@ -44,29 +47,29 @@ displayAll();
 // View All EL
 const all = document.getElementById("all-projects");
 all.addEventListener("click", () => {
-    clearForm();
-    displayAll();
+  clearForm();
+  displayAll();
 });
 
 updateSidebarList();
 
 // Called every time a project is added
 function updateSidebarList() {
-    const projTitleList = document.getElementById("proj-title-list");
-    projTitleList.textContent = " ";
-    allProjects.forEach(proj => {
-        const item = document.createElement("li");
-        const title = document.createElement("span");
-        title.setAttribute("class", "proj-title");
-        title.textContent = proj.getTitle();
-        item.append(title);
+  const projTitleList = document.getElementById("proj-title-list");
+  projTitleList.textContent = " ";
+  allProjects.forEach((proj) => {
+    const item = document.createElement("li");
+    const title = document.createElement("span");
+    title.setAttribute("class", "proj-title");
+    title.textContent = proj.getTitle();
+    item.append(title);
 
-        projTitleList.append(item);
-        item.addEventListener('click', () => {
-            clearForm();
-            projectView(proj);
-        });
+    projTitleList.append(item);
+    item.addEventListener("click", () => {
+      clearForm();
+      projectView(proj);
     });
+  });
 }
 
 export { updateSidebarList };
